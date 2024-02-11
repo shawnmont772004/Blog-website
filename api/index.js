fe725a51a220e34fe07cbc85fafeef9a332b24eb
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-02-11 13:06:02 +0530

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+    console.log("Connected to MongoDB")
+    })
+    .catch((err)=>{
+    console.log("Couldn't connect to database");
+    })
 
 app.listen(3000, () => {
     console.log("Server running at local host:3000!!!")
